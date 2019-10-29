7dd827ea32bde2d5a0400f21cee1226fe18deb28
Max <max@toramax.lvrs.local>
2019-10-29 19:08:50 +0200

    57567

--- 5study.py
+++ 5study.py
@@ -1,7 +1,25 @@
-# Sum
-num_1 = float (input ("Enter first num: "))
-num_2 = float (input ("Enter second num: "))
-res = num_1 + num_2 
-res * 2
-print ("Result is:", res)
-del res
+# = равно
+# == соответствует
+# >= больше равно
+# <= меньше равно
+# != неравно
+
+
+# Мой калькулятор №1
+
+num1 = float( input("Число 1: ") )
+what = input( "Введите операцию (+, -, *, /): " )
+num2 = float( input("Число 2: ") )
+
+if what == "+":
+    res = num1+num2
+    print ("Результат: " + str (res) )
+if what == "-":
+    res = num1-num2
+    print ("Результат: " + str (res) )
+if what == "*":
+    res = num1*num2
+    print ("Результат: " + str (round (res) ) )
+if what == "/":
+    res = num1/num2
+    print ("Результат: " + str (round (res) ) )
