b9eb28264f14d5470cdf5faf8b609090be623215
Max <max@toramax.lvrs.local>
2019-11-05 13:35:18 +0200

    4234

--- 5study.py
+++ 5study.py
@@ -5,21 +5,3 @@
 # != неравно
 
 
-# Мой калькулятор №1
-
-num1 = float( input("Число 1: ") )
-what = input( "Введите операцию (+, -, *, /): " )
-num2 = float( input("Число 2: ") )
-
-if what == "+":
-    res = num1+num2
-    print ("Результат: " + str (res) )
-if what == "-":
-    res = num1-num2
-    print ("Результат: " + str (res) )
-if what == "*":
-    res = num1*num2
-    print ("Результат: " + str (round (res) ) )
-if what == "/":
-    res = num1/num2
-    print ("Результат: " + str (round (res) ) )
--- study.py
+++ study.py
@@ -40,17 +40,19 @@
 
 # Мой калькулятор №1
 
+num1 = float( input("Число 1: ") )
 what = input( "Введите операцию (+, -, *, /): " )
-
-a = float( input("Число 1: ") )
-b = float( input( "Число 2: ") )
+num2 = float( input("Число 2: ") )
 
 if what == "+":
-    c = a + b
-    print ("Результат: " + str (с) )
-elif what == "-":
-    c = a - b
-    print ("Результат: " + str (с) )
-else:
-    print("Выбрана неверная операция")
-    
+    res = num1+num2
+    print ("Результат: " + str (res) )
+if what == "-":
+    res = num1-num2
+    print ("Результат: " + str (res) )
+if what == "*":
+    res = num1*num2
+    print ("Результат: " + str (round (res) ) )
+if what == "/":
+    res = num1/num2
+    print ("Результат: " + str (round (res) ) )
